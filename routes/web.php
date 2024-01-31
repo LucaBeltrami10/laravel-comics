1448a207af0e0b6b912d8e1cdc5110cc82ea8010
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-31 18:14:45 +0100

    inserito contenuto test in home. eliminato products e route per products

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('products', function () {
-    return view('products');
+    return view('pages.home');
 });
