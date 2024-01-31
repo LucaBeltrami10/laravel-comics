cde4bd4b0fe0de57ffc1d7b2ebd8df837a81993d
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-31 18:43:32 +0100

    importato fogli scss da vecchia repo e collegati. aggiunto style a header

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -4,12 +4,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>titolo</title>
+    <title>@yield('title', 'Laravel')</title>
 
     @vite('resources/js/app.js')
 </head>
 
 <body>
+
+    @include('partials.header')
+
+    <h1>app</h1>
 
 </body>
 
