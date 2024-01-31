c3e131636ebe4c01ab2fd94e902937f96a2dc389
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-01-31 18:30:04 +0100

    creato foglio scss per header e footer

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -5,16 +5,16 @@
         </div>
         <nav>
             <ul>
-                <li>characters</li>
-                <li>comics</li>
-                <li>movies</li>
-                <li>tv</li>
-                <li>games</li>
-                <li>collectibiles</li>
-                <li>videos</li>
-                <li>fans</li>
-                <li>news</li>
-                <li>shop</li>
+                <li><a href="#">characters</a></li>
+                <li><a href="#">comics</a></li>
+                <li><a href="#">movies</a></li>
+                <li><a href="#">tv</a></li>
+                <li><a href="#">games</a></li>
+                <li><a href="#">collectibiles</a></li>
+                <li><a href="#">videos</a></li>
+                <li><a href="#">fans</a></li>
+                <li><a href="#">news</a></li>
+                <li><a href="#">shop</a></li>
             </ul>
         </nav>
     </div>
